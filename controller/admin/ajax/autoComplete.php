67897e574e7d71a71dbc247f861195731d63d32d
Manish Shrestha <manish@kazistudios.com>
2023-11-13 13:01:09 +0545

    Adding ajax changes

--- controller/admin/ajax/autoComplete.php
+++ controller/admin/ajax/autoComplete.php
@@ -6,13 +6,13 @@ include "../../../system/config.php";
 if(isset($_POST['term']))
 {	
 	// print_r($_POST);
-	$keyword = mysql_real_escape_string($_POST['term']);
-	$warehouse_id = mysql_real_escape_string($_POST['wid']);
+	$keyword = mysqli_real_escape_string($GLOBALS['mysqli'], $_POST['term']);
+	$warehouse_id = mysqli_real_escape_string($GLOBALS['mysqli'], $_POST['wid']);
 	$qur = "SELECT item_id,item_name, item_qty FROM " . $tableName['item'] . " WHERE item_name LIKE '%". $keyword ."%' and w_id = '" . $warehouse_id . "' and item_qty > 0 ORDER BY item_id ASC LIMIT 0, 20";
-	$result = mysql_query($qur) or die(mysql_errno());
-	if(mysql_num_rows($result)){
+	$result = mysqli_query($GLOBALS['mysqli'], $qur) or die(mysqli_errno());
+	if(mysqli_num_rows($result)){
 		$mainAry = array();
-		while( $row = mysql_fetch_assoc($result) ) {
+		while( $row = $result->fetch_array(MYSQLI_ASSOC) ) {
 			$ary = array();
 			$ary["item_name"] = ucfirst($row["item_name"]);
 			$ary["item_id"] = $row["item_id"];
