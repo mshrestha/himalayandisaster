86161f20f30d2d7a58ca2805caa81cbc7449eaba
Manish Shrestha <manish@kazistudios.com>
2023-11-13 17:52:53 +0545

    Adding autocomplete

--- controller/admin/ajax/autoComplete.php
+++ controller/admin/ajax/autoComplete.php
@@ -7,7 +7,7 @@ if(isset($_POST['term'])){
 	$warehouse_id = mysqli_real_escape_string($GLOBALS['mysqli'], $_POST['wid']);
 	$qur = "SELECT item_id,item_name, item_qty FROM " . $tableName['item'] . " WHERE item_name LIKE '%". $keyword ."%' and w_id = '" . $warehouse_id . "' and item_qty > 0 ORDER BY item_id ASC LIMIT 0, 20";
 	$result = mysqli_query($GLOBALS['mysqli'], $qur) or die(mysqli_errno());
-	var_dump($qur);
+	
 	if(mysqli_num_rows($result)){
 		$mainAry = array();
 		while( $row = $result->fetch_array(MYSQLI_ASSOC) ) {
