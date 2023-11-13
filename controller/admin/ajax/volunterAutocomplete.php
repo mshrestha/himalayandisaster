14c1e9cfd08fe68dd1ed8cfa8775ec9741533013
Manish Shrestha <manish@kazistudios.com>
2023-11-13 12:53:52 +0545

    Adding ajax complete

--- controller/admin/ajax/volunterAutocomplete.php
+++ controller/admin/ajax/volunterAutocomplete.php
@@ -9,10 +9,10 @@ if(isset($_POST['term']))
 	$keyword = $_POST['term'];
 	$qur = "SELECT agent_id, agent_name FROM ".$tableName['agent']." WHERE agent_name LIKE '%". $keyword ."%' and agent_status = 0 ORDER BY agent_name ASC LIMIT 0, 20";
 	
-	$result = mysql_query($qur) or die(mysql_errno());
-	if(mysql_num_rows($result)){
+	$result = mysqli_query($GLOBALS['mysqli'], $qur) or die(mysqli_errno());
+	if(mysqli_num_rows($result)){
 		$mainAry = array();
-		while( $row = mysql_fetch_assoc($result) ) {
+		while( $row = $result->fetch_array(MYSQLI_ASSOC) ) {
 			$ary = array();
 			$ary["agent_name"] = $row["agent_name"];
 			$ary["agent_id"] = $row["agent_id"];
