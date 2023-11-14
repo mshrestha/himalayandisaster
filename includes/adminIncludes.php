4585f523a928f314fbf34ccf26499967cbc556b7
Manish Shrestha <manish@kazistudios.com>
2023-11-14 14:29:59 +0545

    Adding https for all font loads

--- includes/adminIncludes.php
+++ includes/adminIncludes.php
@@ -7,16 +7,16 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Himalayan Disaster</title>
 	<script type="text/javascript" src="../js/html5shiv.js"></script>
-    <link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css' />
-    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css' />
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css' />
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic" rel="stylesheet" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css' />
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css' />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css' />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic" rel="stylesheet" type="text/css" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" href="../css/adminstyle.css">
     <link rel="stylesheet" href="../css/adminstyle2.css">
     <link rel="stylesheet" href="../css/ui.css">
-    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+    <link rel="stylesheet" href="https://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<META name="description" content="Save Nepal" />
 	<META name="keywords" content="Nepal, prayfornepal, savenepal, earthquake, earthquakerelif" /> 
