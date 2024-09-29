ac0cbb8d8bfc8ddbaf2cf84b743a991c70f23632
Manish Shrestha <manish@kazistudios.com>
2024-09-29 08:02:25 +0545

    Change

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ include("includes/header.php");
 				$where="";
 			}
 			$qur = "select w.w_name,a.help_call_latlng,a.help_call_id,a.pkg_count,a.pkg_id,a.pkg_count,a.pkg_timestamp,a.pkg_approval,a.help_call_latlng,b.vdc_name, b.district, c.agent_name,c.agent_email,c.agent_phone, a.w_id
-					from ". $tableName['package'] ." a," . $tableName['vdc'] . " b," .$tableName['agent'] ." c, ". $tableName['warehouse'] . " w" .
+					from ". $tableName['package'] ." a," . $tableName['vdc'] . " b," .$tableName['agent'] ." c, ". $tableName['warehouse'] . " w " .
 					"where $where a.agent_id=c.agent_id and w.w_id = a.w_id and a.help_call_id=b.vdc_code ". $whereCondition . " order by a.pkg_count ASC" . $offset;
             // die($qur);
             $addressPoints = '';
